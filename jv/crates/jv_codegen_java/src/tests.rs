21270c39696118465906be550dbee23c96526c02
asopitech <asopitech.iot.research+github@gmail.com>
2025-09-19 11:47:17 +0900

    test(jv_codegen_java): add IR and Java snapshots

--- jv/crates/jv_codegen_java/src/tests.rs
+++ jv/crates/jv_codegen_java/src/tests.rs
@@ -1,9 +1,11 @@
 use super::*;
+use insta::assert_snapshot;
 use jv_ast::{Literal, Span};
 use jv_ir::{
     IrExpression, IrModifiers, IrParameter, IrProgram, IrStatement, IrVisibility, JavaType,
     MethodOverload,
 };
+use serde_json::to_string_pretty;
 
 fn dummy_span() -> Span {
     Span::dummy()
@@ -84,6 +86,124 @@ fn simple_class() -> IrStatement {
             visibility: IrVisibility::Public,
             ..IrModifiers::default()
         },
+        span: dummy_span(),
+    }
+}
+
+fn snapshot_program() -> IrProgram {
+    let mut block_statements = Vec::new();
+
+    block_statements.push(IrStatement::VariableDeclaration {
+        name: "message".to_string(),
+        java_type: string_type(),
+        initializer: Some(IrExpression::Literal(
+            Literal::String("Hello".to_string()),
+            dummy_span(),
+        )),
+        is_final: true,
+        modifiers: IrModifiers::default(),
+        span: dummy_span(),
+    });
+
+    let comparison = IrExpression::Binary {
+        left: Box::new(IrExpression::Identifier {
+            name: "input".to_string(),
+            java_type: string_type(),
+            span: dummy_span(),
+        }),
+        op: jv_ast::BinaryOp::NotEqual,
+        right: Box::new(IrExpression::Literal(
+            Literal::String("".to_string()),
+            dummy_span(),
+        )),
+        java_type: JavaType::Primitive("boolean".to_string()),
+        span: dummy_span(),
+    };
+
+    let then_block = IrStatement::Block {
+        statements: vec![IrStatement::Return {
+            value: Some(IrExpression::Identifier {
+                name: "input".to_string(),
+                java_type: string_type(),
+                span: dummy_span(),
+            }),
+            span: dummy_span(),
+        }],
+        span: dummy_span(),
+    };
+
+    let else_block = IrStatement::Block {
+        statements: vec![IrStatement::Return {
+            value: Some(IrExpression::Identifier {
+                name: "message".to_string(),
+                java_type: string_type(),
+                span: dummy_span(),
+            }),
+            span: dummy_span(),
+        }],
+        span: dummy_span(),
+    };
+
+    block_statements.push(IrStatement::If {
+        condition: comparison,
+        then_stmt: Box::new(then_block),
+        else_stmt: Some(Box::new(else_block)),
+        span: dummy_span(),
+    });
+
+    block_statements.push(IrStatement::Return {
+        value: Some(IrExpression::Identifier {
+            name: "message".to_string(),
+            java_type: string_type(),
+            span: dummy_span(),
+        }),
+        span: dummy_span(),
+    });
+
+    let method_body = IrExpression::Block {
+        statements: block_statements,
+        java_type: string_type(),
+        span: dummy_span(),
+    };
+
+    let method = IrStatement::MethodDeclaration {
+        name: "greet".to_string(),
+        parameters: vec![IrParameter {
+            name: "input".to_string(),
+            java_type: string_type(),
+            modifiers: IrModifiers::default(),
+            span: dummy_span(),
+        }],
+        return_type: string_type(),
+        body: Some(method_body),
+        modifiers: IrModifiers {
+            visibility: IrVisibility::Public,
+            is_static: true,
+            ..IrModifiers::default()
+        },
+        throws: vec![],
+        span: dummy_span(),
+    };
+
+    let class = IrStatement::ClassDeclaration {
+        name: "Greeter".to_string(),
+        type_parameters: vec![],
+        superclass: None,
+        interfaces: vec![],
+        fields: vec![],
+        methods: vec![method],
+        nested_classes: vec![],
+        modifiers: IrModifiers {
+            visibility: IrVisibility::Public,
+            ..IrModifiers::default()
+        },
+        span: dummy_span(),
+    };
+
+    IrProgram {
+        package: Some("example.greeter".to_string()),
+        imports: vec![],
+        type_declarations: vec![class],
         span: dummy_span(),
     }
 }
@@ -175,4 +295,355 @@ fn method_overload_generation_wraps_body() {
     assert_eq!(overloads.len(), 1);
     assert!(overloads[0].contains("public int sum"));
     assert!(overloads[0].contains("return a;"));
+}
+
+#[test]
+fn snapshot_ir_program() {
+    let program = snapshot_program();
+    let json = to_string_pretty(&program).expect("serialise IR program");
+    assert_snapshot!(
+        json.as_str(),
+        @r###"{
+  "package": "example.greeter",
+  "imports": [],
+  "type_declarations": [
+    {
+      "ClassDeclaration": {
+        "name": "Greeter",
+        "type_parameters": [],
+        "superclass": null,
+        "interfaces": [],
+        "fields": [],
+        "methods": [
+          {
+            "MethodDeclaration": {
+              "name": "greet",
+              "parameters": [
+                {
+                  "name": "input",
+                  "java_type": {
+                    "Reference": {
+                      "name": "String",
+                      "generic_args": []
+                    }
+                  },
+                  "modifiers": {
+                    "visibility": "Package",
+                    "is_static": false,
+                    "is_final": false,
+                    "is_abstract": false,
+                    "is_synchronized": false,
+                    "is_native": false,
+                    "is_strictfp": false,
+                    "annotations": []
+                  },
+                  "span": {
+                    "start_line": 0,
+                    "start_column": 0,
+                    "end_line": 0,
+                    "end_column": 0
+                  }
+                }
+              ],
+              "return_type": {
+                "Reference": {
+                  "name": "String",
+                  "generic_args": []
+                }
+              },
+              "body": {
+                "Block": {
+                  "statements": [
+                    {
+                      "VariableDeclaration": {
+                        "name": "message",
+                        "java_type": {
+                          "Reference": {
+                            "name": "String",
+                            "generic_args": []
+                          }
+                        },
+                        "initializer": {
+                          "Literal": [
+                            {
+                              "String": "Hello"
+                            },
+                            {
+                              "start_line": 0,
+                              "start_column": 0,
+                              "end_line": 0,
+                              "end_column": 0
+                            }
+                          ]
+                        },
+                        "is_final": true,
+                        "modifiers": {
+                          "visibility": "Package",
+                          "is_static": false,
+                          "is_final": false,
+                          "is_abstract": false,
+                          "is_synchronized": false,
+                          "is_native": false,
+                          "is_strictfp": false,
+                          "annotations": []
+                        },
+                        "span": {
+                          "start_line": 0,
+                          "start_column": 0,
+                          "end_line": 0,
+                          "end_column": 0
+                        }
+                      }
+                    },
+                    {
+                      "If": {
+                        "condition": {
+                          "Binary": {
+                            "left": {
+                              "Identifier": {
+                                "name": "input",
+                                "java_type": {
+                                  "Reference": {
+                                    "name": "String",
+                                    "generic_args": []
+                                  }
+                                },
+                                "span": {
+                                  "start_line": 0,
+                                  "start_column": 0,
+                                  "end_line": 0,
+                                  "end_column": 0
+                                }
+                              }
+                            },
+                            "op": "NotEqual",
+                            "right": {
+                              "Literal": [
+                                {
+                                  "String": ""
+                                },
+                                {
+                                  "start_line": 0,
+                                  "start_column": 0,
+                                  "end_line": 0,
+                                  "end_column": 0
+                                }
+                              ]
+                            },
+                            "java_type": {
+                              "Primitive": "boolean"
+                            },
+                            "span": {
+                              "start_line": 0,
+                              "start_column": 0,
+                              "end_line": 0,
+                              "end_column": 0
+                            }
+                          }
+                        },
+                        "then_stmt": {
+                          "Block": {
+                            "statements": [
+                              {
+                                "Return": {
+                                  "value": {
+                                    "Identifier": {
+                                      "name": "input",
+                                      "java_type": {
+                                        "Reference": {
+                                          "name": "String",
+                                          "generic_args": []
+                                        }
+                                      },
+                                      "span": {
+                                        "start_line": 0,
+                                        "start_column": 0,
+                                        "end_line": 0,
+                                        "end_column": 0
+                                      }
+                                    }
+                                  },
+                                  "span": {
+                                    "start_line": 0,
+                                    "start_column": 0,
+                                    "end_line": 0,
+                                    "end_column": 0
+                                  }
+                                }
+                              }
+                            ],
+                            "span": {
+                              "start_line": 0,
+                              "start_column": 0,
+                              "end_line": 0,
+                              "end_column": 0
+                            }
+                          }
+                        },
+                        "else_stmt": {
+                          "Block": {
+                            "statements": [
+                              {
+                                "Return": {
+                                  "value": {
+                                    "Identifier": {
+                                      "name": "message",
+                                      "java_type": {
+                                        "Reference": {
+                                          "name": "String",
+                                          "generic_args": []
+                                        }
+                                      },
+                                      "span": {
+                                        "start_line": 0,
+                                        "start_column": 0,
+                                        "end_line": 0,
+                                        "end_column": 0
+                                      }
+                                    }
+                                  },
+                                  "span": {
+                                    "start_line": 0,
+                                    "start_column": 0,
+                                    "end_line": 0,
+                                    "end_column": 0
+                                  }
+                                }
+                              }
+                            ],
+                            "span": {
+                              "start_line": 0,
+                              "start_column": 0,
+                              "end_line": 0,
+                              "end_column": 0
+                            }
+                          }
+                        },
+                        "span": {
+                          "start_line": 0,
+                          "start_column": 0,
+                          "end_line": 0,
+                          "end_column": 0
+                        }
+                      }
+                    },
+                    {
+                      "Return": {
+                        "value": {
+                          "Identifier": {
+                            "name": "message",
+                            "java_type": {
+                              "Reference": {
+                                "name": "String",
+                                "generic_args": []
+                              }
+                            },
+                            "span": {
+                              "start_line": 0,
+                              "start_column": 0,
+                              "end_line": 0,
+                              "end_column": 0
+                            }
+                          }
+                        },
+                        "span": {
+                          "start_line": 0,
+                          "start_column": 0,
+                          "end_line": 0,
+                          "end_column": 0
+                        }
+                      }
+                    }
+                  ],
+                  "java_type": {
+                    "Reference": {
+                      "name": "String",
+                      "generic_args": []
+                    }
+                  },
+                  "span": {
+                    "start_line": 0,
+                    "start_column": 0,
+                    "end_line": 0,
+                    "end_column": 0
+                  }
+                }
+              },
+              "modifiers": {
+                "visibility": "Public",
+                "is_static": true,
+                "is_final": false,
+                "is_abstract": false,
+                "is_synchronized": false,
+                "is_native": false,
+                "is_strictfp": false,
+                "annotations": []
+              },
+              "throws": [],
+              "span": {
+                "start_line": 0,
+                "start_column": 0,
+                "end_line": 0,
+                "end_column": 0
+              }
+            }
+          }
+        ],
+        "nested_classes": [],
+        "modifiers": {
+          "visibility": "Public",
+          "is_static": false,
+          "is_final": false,
+          "is_abstract": false,
+          "is_synchronized": false,
+          "is_native": false,
+          "is_strictfp": false,
+          "annotations": []
+        },
+        "span": {
+          "start_line": 0,
+          "start_column": 0,
+          "end_line": 0,
+          "end_column": 0
+        }
+      }
+    }
+  ],
+  "span": {
+    "start_line": 0,
+    "start_column": 0,
+    "end_line": 0,
+    "end_column": 0
+  }
+}
+"###
+    );
+}
+
+#[test]
+fn snapshot_java_output() {
+    let program = snapshot_program();
+    let source = generate_java_source(&program).expect("java generation");
+    assert_snapshot!(
+        source.as_str(),
+        @r###"package example.greeter;
+
+public class Greeter {
+    public static String greet(String input) {
+        final String message = "Hello";
+        if (input != "") {
+            {
+                return input;
+            }
+        } else {
+            {
+                return message;
+            }
+        }
+        return message;
+    }
+    
+}
+"###
+    );
 }
